5ba184c1a726ee64034fc025bff1d788bf4b40ae
Dat Phan Minh <darkstardragon9@gmail.com>
2022-12-08 20:23:39 +0700

    reup

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-
-class SignUpForm(FlaskForm):
-    username = StringField(label="username")
-    password1 = PasswordField(label="password1")
-    password2 = PasswordField(label="password2")
-    submit = SubmitField(label="submit")
