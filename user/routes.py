66370a8c5864ce72b8e835264555d83c55e61b23
Dat Phan Minh <darkstardragon9@gmail.com>
2022-12-09 15:42:37 +0700

    add icon to title

--- user/routes.py
+++ user/routes.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, request,redirect, flash
 from app import app
 from .models import User
-
+from time import sleep
 @app.route('/register',methods=['GET','POST'])
 def register_page():
     if request.method == 'POST':
         msg,status = User().signup()
+        flash(msg)
         if (status != 200):
-            print(status)
             flash(msg)
+            print(status)
         else:
             return redirect("/login")
     return render_template("register.html")
